067f1edc27df7ae493058c917d5461f83d66b002
farrux1994 <f.turdiyevbek94@gmail.com>
2023-01-17 14:38:24 +0500

    begin09

--- begin09.py
+++ begin09.py
@@ -7,6 +7,6 @@ c = 2
 # Assign 4 (int value) to 'd' variable;
 d = 4
 # Assign the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin09' to variable 'k';
-k = 'https://github.com/codeschool-2022A/variables_and_types#begin09'
+k = a+b-c*d
 # # Print the variable 'k'.
 print(k)
