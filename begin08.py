501d5a1aced9abfc5eec4f9d59448888a70a2c54
farrux1994 <f.turdiyevbek94@gmail.com>
2023-01-17 14:36:12 +0500

    begin08

--- begin08.py
+++ begin08.py
@@ -5,4 +5,4 @@ b = 4
 # Assign 8 (int value) to 'c' variable;
 c = 8
 # Print the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin08';
-print('https://github.com/codeschool-2022A/variables_and_types#begin08')
+print((a*b)/c)
